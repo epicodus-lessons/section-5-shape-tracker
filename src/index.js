b0b1e1341ff3b2a92ae0d30e2b11240963db2295
Brooke Kullberg <brookekullberg@gmail.com>
2022-07-07 13:54:06 -0700

    add working project

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Triangle from './triangle.js';
+
+function handleTriangleForm() {
+  event.preventDefault();
+  document.querySelector('#response').innerText = null;
+  const length2 = parseInt(document.querySelector('#length2').value);
+  const length1 = parseInt(document.querySelector('#length1').value);
+  const length3 = parseInt(document.querySelector('#length3').value);
+  const triangle = new Triangle(length1, length2, length3);
+  const response = triangle.checkType();
+  const pTag = document.createElement("p");
+  pTag.append(response);
+  document.querySelector('#response').append(pTag);
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("#triangle-checker-form").addEventListener("submit", handleTriangleForm);
+});
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {               
+    contentBase: './dist'    
+  },
+  plugins: [
+    new ESLintPlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Shape Tracker',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
